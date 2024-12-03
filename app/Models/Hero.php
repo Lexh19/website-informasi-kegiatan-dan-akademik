456f6d95d866925100355966a3949753030153f1
Lexh19 <iqbaalhzd@gmail.com>
2024-12-04 00:21:49 +0700

    create tabel heroes

---
+++ app/Models/Hero.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Hero extends Model
+{
+    use HasFactory;
+    protected $fillable = ['title', 'subtitle'];
+
+
+    public function getSubtitleAttribute($value)
+    {
+        return strip_tags($value);
+    }
+
+}
