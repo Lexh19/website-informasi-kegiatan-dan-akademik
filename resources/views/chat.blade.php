02ff836de7991e4ccc47f0cc27ed9a3a19e86c42
Lexh19 <iqbaalhzd@gmail.com>
2025-05-23 16:58:57 +0700

    feat page chat bot

---
+++ resources/views/chat.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+
+<section>
+    <div class="container py-5">
+
+        <div class="row d-flex justify-content-center">
+            <div class="col-md-10 col-lg-8 col-xl-6">
+
+            </div>
+        </div>
+
+    </div>
+</section>
+</html>
