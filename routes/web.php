5c179aaf5a88f4bad515f990800343a0f2b227e4
Lexh19 <iqbaalhzd@gmail.com>
2025-05-31 18:26:27 +0700

    style : router chat bot

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,18 @@
 use App\Models\Hero;
 use App\Models\About;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ChatbotController;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 
 Route::get('/', function () {
     $heros = Hero::all();
     $abouts = About::all();
-
-
 
     return view('welcome', [
         'heros' => $heros,
         'abouts' => $abouts
     ]);
 });
+
+Route::post('/chat', ChatbotController::class)
+    ->withoutMiddleware([VerifyCsrfToken::class]); 
